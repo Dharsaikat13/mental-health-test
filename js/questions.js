da3d6b65787bfe9f169e1fafcea93e1bb9c0da8b
SAIKAT <37828689+Dharsaikat13@users.noreply.github.com>
2021-05-27 20:47:19 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,54 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "what are You feeling Now?",
+    answer: "Feeliing Bored",
+    options: [
+      "Feeling Bored",
+      "Feeling Well",
+     
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are Issues You Facing?",
+    answer: "Getting Headach",
+    options: [
+      "Getting Headach",
+      "Nothing Much",
+      
+    ]
+  },
+    {
+    numb: 3,
+    question: "Are You Feeling Sleepy?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      
+    ]
+  },
+    {
+    numb: 4,
+    question: "Have Your mood changes of highs and lows?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      
+    ]
+  },
+    {
+    numb: 5,
+    question: "Have You Suicidal Thinking In Your Mind?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      
+    ]
+  },
+  
+];
